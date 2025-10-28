78b39c64150c0294663f5c6b54b889441109b361
BenZdunczuk <b.zdunczuk@gmail.com>
2025-10-28 18:47:31 +0100

    Add process file function from pc

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include "PPFunc.hh"
+#include <sstream>
+#include <iostream>
+
+
+bool preprocesFile(const char* fileName, std::istringstream &stream){
+
+    std::string cmd= "cpp -P ";
+    char line[LINE_SIZE];
+    std::ostringstream tmp;
+
+    cmd+= fileName;
+    FILE* pProc = popen(cmd.c_str(), "r");
+
+    if (!pProc) return false;
+
+    while (fgets(line, LINE_SIZE, pProc))
+    {
+        tmp << line;
+    }
+    stream.str(tmp.str());
+
+    return pclose(pProc)==0;
+}
